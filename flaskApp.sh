37380262020566990aafd48beee6c3e0110d7000
Boyd Ndonga <boydbugle@gmail.com>
2018-09-14 13:04:49 +0300

    createinitial script execute point

--- flaskApp.sh
+++ flaskApp.sh
@@ -1,1 +1,24 @@
 #!/usr/bin/env bash
+
+
+# Creating new app
+
+
+# Initializing  git
+git init
+touch .gitignore
+echo "should i create a README? y/n"
+read answer
+if answer == y; then
+    touch README.md
+    echo "created README"
+fi
+
+
+cat >> .gitignore << EOF
+
+virtual/
+*.pyc
+start.sh
+
+EOF
