9c4a3c7fda447ccae35cc4c456de9882bf1486e6
Boyd Ndonga <boydbugle@gmail.com>
2018-09-14 21:53:20 +0300

    create select options for editor

--- flaskApp.sh
+++ flaskApp.sh
@@ -409,3 +409,24 @@ EOF
 
 # Creating initial commit
 git add . && git commit -m "Initial Commit"
+
+PS3='Please enter your choice: '
+options=("Option 1" "Option 2" "Option 3" "Quit")
+select opt in "${options[@]}"
+do
+    case $opt in
+        "Option 1")
+            echo "you chose choice 1"
+            ;;
+        "Option 2")
+            echo "you chose choice 2"
+            ;;
+        "Option 3")
+            echo "you chose choice $REPLY which is $opt"
+            ;;
+        "Quit")
+            break
+            ;;
+        *) echo "invalid option $REPLY";;
+    esac
+done
