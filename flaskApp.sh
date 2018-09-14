473d7bb59089f1e816ca113870ab8dc9bd98a20d
Boyd Ndonga <boydbugle@gmail.com>
2018-09-14 17:08:13 +0300

    configure shell with db and migration

--- flaskApp.sh
+++ flaskApp.sh
@@ -171,12 +171,24 @@ import unittest
 from app import create_app,db
 from flask_script import Manager,Server
 from app import create_app,db
+from  flask_migrate import Migrate, MigrateCommand
 
 app = create_app('default')
 
 manager = Manager(app)
-
+migrate = Migrate(app,db)
 manager.add_command('server', Server)
+manager.add_command('db',MigrateCommand)
+
+@manager.command
+def test():
+    """Run the unit tests."""
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
+@manager.shell
+def make_shell_context():
+    return dict(app = app,db = db)
 
 if __name__ == '__main__':
     manager.run()
