6ff39991f26c9c52518eb1f219e9cec7a33e73a9
Boyd Ndonga <boydbugle@gmail.com>
2018-09-14 15:55:22 +0300

    create config environments

--- flaskApp.sh
+++ flaskApp.sh
@@ -149,9 +149,12 @@ class Config:
 class ProdConfig(Config):
     pass
 
+class TestConfig(Config):
+    pass
+
 class DevConfig(Config):
     DEBUG = True
 
-config_options ={"production":ProdConfig,"default":DevConfig}
+config_options ={"production":ProdConfig,"default":DevConfig,"testing":TestConfig}
 
 EOF
